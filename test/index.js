70d2b2c0e752946afc2435905af4de739b9742db
Marcelo Boeira <me@marceloboeira.com>
2019-01-18 15:42:00 +0100

    Improve tests responsibilities
    The main module test doesn't need to test full implementation, since the
    logger tests are covering that end now

--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ var chai = require("chai")
 var mochaSinon = require("mocha-sinon")
 var expect = require("chai").expect
 var metrics = require("../src/index")
+var logger = require("../src/logger")
 
 chai.use(require("sinon-chai"))
 chai.use(require("chai-match"))
@@ -13,44 +14,30 @@ describe("aws-datadog-metrics", function() {
   beforeEach(function() {
     // we freeze time and spy the console to make it easier to test
     this.clock = sinon.useFakeTimers(new Date(2018, 4, 8).getTime())
-    this.sinon.spy(console, "log")
-  })
-
-  describe("basic functionality", function() {
-    it("formats the message properly", function() {
-      metrics.increment("foo")
-
-      expect(console.log.getCall(0).args[0]).to.match(/MONITORING\|\d+\|1\|count\|foo\|#/)
-    })
-
-    it("joins the tags", function() {
-      metrics.increment("foo.bar", 1, ["tag:value", "another:tag"])
-
-      expect(console.log.getCall(0).args[0]).to.match(/MONITORING\|\d+\|1\|count\|foo\.bar\|#tag:value,another:tag/)
-    })
+    this.sinon.spy(logger, "log")
   })
 
   describe("increment", function() {
-    it("uses the count keyword as the name", function() {
+    it("uses the count keyword as the type", function() {
       metrics.increment("foo", 23, ["m:a"])
 
-      expect(console.log.getCall(0).args[0]).to.match(/MONITORING\|\d+\|23\|count\|foo\|#m:a/)
+      expect(logger.log.getCall(0).args).to.eql(["count", "foo", 23, ["m:a"]])
     })
   })
 
   describe("gauge", function() {
-    it("uses the gauge keyword as the name", function() {
+    it("uses the gauge keyword as the type", function() {
       metrics.gauge("foo", 23, ["a:b"])
 
-      expect(console.log.getCall(0).args[0]).to.match(/MONITORING\|\d+\|23\|gauge\|foo\|#a:b/)
+      expect(logger.log.getCall(0).args).to.eql(["gauge", "foo", 23, ["a:b"]])
     })
   })
 
   describe("histogram", function() {
-    it("uses the histogram keyword as the name", function() {
+    it("uses the histogram keyword as the type", function() {
       metrics.histogram("foo", 29, ["d:e"])
 
-      expect(console.log.getCall(0).args[0]).to.match(/MONITORING\|\d+\|29\|histogram\|foo\|#d:e/)
+      expect(logger.log.getCall(0).args).to.eql(["histogram", "foo", 29, ["d:e"]])
     })
   })
 })
